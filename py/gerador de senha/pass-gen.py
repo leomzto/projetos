cfc394f2fa61bf72f281ccd5a487c6f86f6c0cbd
Leo Manzato <leopmanzato@gmail.com>
2025-05-31 01:58:58 -0300

    conversor monetario em py

--- py/gerador de senha/pass-gen.py
+++ py/gerador de senha/pass-gen.py
@@ -3,7 +3,7 @@ import string
 
 def gerar_senha(tamanho=12):
     caracteres = string.ascii_letters + string.digits + string.punctuation
-    senha = ''.join(random.choice(caracteres) for _ in range(tamanho))
+    senha = ''.join(random.choice(caracteres) for i in range(tamanho))
     return senha
 
 tamanho = int(input('Digite o tamanho da senha: '))
