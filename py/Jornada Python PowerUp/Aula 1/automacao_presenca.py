b7e1fe4e3e39396d063afae40524b1419b1b6f29
Leo Manzato <leopmanzato@gmail.com>
2025-06-02 22:44:06 -0300

    automacao de presença - Jornada Python

--- py/Jornada Python PowerUp/Aula 1/automacao_presenca.py
+++ py/Jornada Python PowerUp/Aula 1/automacao_presenca.py
@@ -1,37 +1,40 @@
 import pyautogui
 from time import sleep
-# CODIGO AULA 1: usepython
-# CODIGO AULA 2:
-# CODIGO AULA 3:
-# CODIGO AULA 4:
 
-AULA = 1
+AULA = numero da aula
 SITE = 'https://hashaqui.com/certificados/jornada-python'
-CODIGO = ''
-EMAIL = 'leopmanzato@gmail.com'
-NOME = 'Leonardo Padovani Manzato'
+CODIGO = 'Codigo da aula'
+EMAIL = 'Email para envio do certificado'
+NOME = 'Nome a ser exibido no certificado'
 
+# ABRIR O NAVEGADOR
 pyautogui.press('win')
 pyautogui.write('chrome')
 pyautogui.press('enter')
 sleep(2)
+# ACESSAR O SITE
 pyautogui.write(SITE)
 pyautogui.press('enter')
 sleep(2)
+# SELECIONAR A AULA
 pyautogui.press('tab')
 pyautogui.press('enter')
 pyautogui.press('down', presses=AULA)
 pyautogui.press('enter')
 sleep(0.75)
+# INSERIR O CODIGO DA AULA
 pyautogui.press('tab')
 pyautogui.write(CODIGO)
 sleep(0.75)
+#INSERIR O EMAIL
 pyautogui.press('tab')
 pyautogui.write(EMAIL)
 sleep(0.75)
+# INSERIR O NOME
 pyautogui.press('tab')
 pyautogui.write(NOME)
 sleep(0.75)
+# ENVIAR
 pyautogui.press('tab')
 pyautogui.press('enter')
 
